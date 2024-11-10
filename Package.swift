186f7e8625b9df9e9f84fa3040892816702ebee2
Alexey <alexeykostenko98@gmail.com>
2024-11-10 14:32:37 +0300

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -6,7 +6,6 @@ let package = Package(
     defaultLocalization: "en",
     platforms: [.iOS(.v14)],
     products: [
-        // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(name: "LivetexUICore", targets: ["LivetexUICore"]),
     ],
     dependencies: [
