954dd421c2d1abc07e800b9a6e2706ee14aa9c7a
Alexey Kostenko <alexeykostenko98@gmail.com>
2024-11-10 13:17:04 +0300

    updated dependencies

--- Package.swift
+++ Package.swift
@@ -10,12 +10,12 @@ let package = Package(
         .library(name: "LivetexUICore", targets: ["LivetexUICore"]),
     ],
     dependencies: [
-        .package(url: "https://github.com/LiveTex/sdk-ios", branch: "master"),
+        .package(url: "https://github.com/LiveTex/sdk-ios", exact: "3.0.3"),
         .package(url: "https://github.com/MessageKit/MessageKit", from: "4.1.1"),
         .package(url: "https://github.com/onevcat/Kingfisher.git", from: "7.9.1" ),
         .package(url: "https://github.com/apvex/BFRImageViewer", branch: "main"),
         .package(url: "https://github.com/pinterest/PINRemoteImage", branch: "master"),
-        .package(url: "https://github.com/evgenyneu/keychain-swift", from: "19.0.0")
+        .package(url: "https://github.com/evgenyneu/keychain-swift", from: "24.0.0")
     ],
     targets: [
         .target(
